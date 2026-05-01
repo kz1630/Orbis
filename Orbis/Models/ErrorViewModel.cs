10579aba25451d29ed85c11f3aea5361b51ff429
Julfy0228 <julfy0228@gmail.com>
2026-05-01 09:34:49 +0300

    Добавьте файлы проекта.

---
+++ Orbis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Orbis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
